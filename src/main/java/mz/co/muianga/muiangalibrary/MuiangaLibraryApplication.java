d8c0e7ad57074a5d64f8f4bea25238a3b2aa0407
Nilvandro Muianga <nilvandro.muianga@vm.co.mz>
2021-01-08 18:08:59 +0200

    Initial commit

---
+++ src/main/java/mz/co/muianga/muiangalibrary/MuiangaLibraryApplication.java
@@ -0,0 +1,13 @@
+package mz.co.muianga.muiangalibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuiangaLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuiangaLibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mz/co/muianga/muiangalibrary/MuiangaLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package mz.co.muianga.muiangalibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuiangaLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
