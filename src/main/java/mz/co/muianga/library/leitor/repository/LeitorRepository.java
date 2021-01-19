e623dad3123d27809d96b074fa70251a64bb3dce
Nilvandro Muianga <nilvandro.muianga@vm.co.mz>
2021-01-19 21:51:22 +0200

    Creating Repository for Leitor

---
+++ src/main/java/mz/co/muianga/library/leitor/repository/LeitorRepository.java
@@ -0,0 +1,7 @@
+package mz.co.muianga.library.leitor.repository;
+
+import mz.co.muianga.library.leitor.model.Leitor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LeitorRepository extends JpaRepository<Leitor, Long> {
+}
